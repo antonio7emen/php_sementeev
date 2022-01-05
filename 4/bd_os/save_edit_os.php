ffebcdb509fe71bff8e645e59feceecdc8795f47
antonio7emen <96733567+antonio7emen@users.noreply.github.com>
2022-01-05 20:42:12 +0500

    123

--- 4/bd_os/save_edit_os.php
+++ 4/bd_os/save_edit_os.php
@@ -3,7 +3,7 @@
 <html><body>
 <?php
  $conn = mysqli_connect("eu-cdbr-west-01.cleardb.com","b82a476b3b9e9d","0de723ba", "heroku_3e0e4fe3001638d") or die ("Невозможно подключиться к серверу");
- mysqli_query('SET NAMES cp1251');
+ mysqli_query($conn , 'SET NAMES cp1251');
  $zapros="UPDATE os SET name='".$_GET['name'].
 "', equipment='".$_GET['equipment']."', bitness='"
 .$_GET['bitness']."', developer='".$_GET['developer'].
