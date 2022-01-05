a039e4a6e71d8f12a4f6c7f5a1c313b16df8e51c
antonio7emen <96733567+antonio7emen@users.noreply.github.com>
2022-01-05 20:54:35 +0500

    123

--- 4/bd_os/save_edit_dk.php
+++ 4/bd_os/save_edit_dk.php
@@ -3,7 +3,7 @@
 <html><body>
 <?php
  $conn = mysqli_connect("eu-cdbr-west-01.cleardb.com","b82a476b3b9e9d","0de723ba", "heroku_3e0e4fe3001638d") or die ("Невозможно подключиться к серверу");
- mysqli_query('SET NAMES cp1251');
+ mysqli_query($conn, 'SET NAMES cp1251');
  $zapros="UPDATE dk SET `date_in`='".$_GET['date_in']. "', `date_out`='".$_GET['date_out']."', `id_os`='".$_GET['id_os']."', `id_ds`='".$_GET['id_ds']."', `price`='".$_GET['price']."', `key`='".$_GET['key']."' WHERE `id`=".$_GET['id'];
  mysqli_query($conn, $zapros);
 if (mysqli_affected_rows($conn)>0) {
