cfdfffc1463d53e186037be1b5b8ab61dd9998fb
antonio7emen <96733567+antonio7emen@users.noreply.github.com>
2022-01-05 20:22:07 +0500

    123

--- 4/bd_os/save_new_os.php
+++ 4/bd_os/save_new_os.php
@@ -1,3 +1,5 @@
+<?php header('Content-Type: text/html; charset=windows-1251'); ?>
+
 <?php
  // Подключение к базе данных:
  $conn = mysqli_connect("eu-cdbr-west-01.cleardb.com","b82a476b3b9e9d","0de723ba", "heroku_3e0e4fe3001638d") or die ("Невозможно подключиться к серверу");
