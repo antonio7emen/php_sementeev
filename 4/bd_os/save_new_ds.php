91afb866ed05a263d5f293f7d2a8087e91e59ebf
antonio7emen <96733567+antonio7emen@users.noreply.github.com>
2022-01-05 20:27:54 +0500

    123

--- 4/bd_os/save_new_ds.php
+++ 4/bd_os/save_new_ds.php
@@ -3,12 +3,12 @@
 <?php
  // Подключение к базе данных:
  $conn = mysqli_connect("eu-cdbr-west-01.cleardb.com","b82a476b3b9e9d","0de723ba", "heroku_3e0e4fe3001638d") or die ("Невозможно подключиться к серверу");
- mysql_query($conn, 'SET NAMES cp1251'); // Тип кодировки
+ mysqli_query($conn, 'SET NAMES cp1251'); // Тип кодировки
  // Строка запроса на добавление записи в таблицу:
  $sql_add = "INSERT INTO ds SET name='" . $_GET['name']
 ."', url='".$_GET['url'] . "'";
- mysql_query($conn, $sql_add); // Выполнение запроса
- if (mysql_affected_rows()>0) // если нет ошибок при выполнении запроса
+ mysqli_query($conn, $sql_add); // Выполнение запроса
+ if (mysqli_affected_rows()>0) // если нет ошибок при выполнении запроса
  { print "<p>Запись сохранена.";
  print "<p><a href=\"index.php\"> Вернуться к списку
 пользователей </a>"; }
--- 4/bd_os/save_new_os.php
+++ 4/bd_os/save_new_os.php
@@ -3,14 +3,14 @@
 <?php
  // Подключение к базе данных:
  $conn = mysqli_connect("eu-cdbr-west-01.cleardb.com","b82a476b3b9e9d","0de723ba", "heroku_3e0e4fe3001638d") or die ("Невозможно подключиться к серверу");
- mysql_query($conn, "SET NAMES cp1251"); // Тип кодировки
+ mysqli_query($conn, "SET NAMES cp1251"); // Тип кодировки
  // Строка запроса на добавление записи в таблицу:
  $sql_add = "INSERT INTO os SET name='" . $_GET['name']
 ."', equipment='".$_GET['equipment']."', bitness='"
 .$_GET['bitness']."', developer='".$_GET['developer'].
 "', population='".$_GET['population']. "'";
- mysql_query($conn, $sql_add); // Выполнение запроса
- if (mysql_affected_rows()>0) // если нет ошибок при выполнении запроса
+ mysqli_query($conn, $sql_add); // Выполнение запроса
+ if (mysqli_affected_rows()>0) // если нет ошибок при выполнении запроса
  { print "<p>Запись сохранена.";
  print "<p><a href=\"index.php\"> Вернуться </a>"; }
  else { print "Ошибка сохранения. <a href=\"index.php\">
