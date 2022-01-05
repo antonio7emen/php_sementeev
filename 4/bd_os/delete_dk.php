3d7bc529775eb565de31b08a2cb208ba11439bbd
antonio7emen <96733567+antonio7emen@users.noreply.github.com>
2022-01-05 20:50:28 +0500

    123

--- 4/bd_os/delete_dk.php
+++ 4/bd_os/delete_dk.php
@@ -1,5 +1,5 @@
 <?php
- mysqli_connect("eu-cdbr-west-01.cleardb.com","b82a476b3b9e9d","0de723ba") or die ("Невозможно подключиться к серверу");
+ $conn = mysqli_connect("eu-cdbr-west-01.cleardb.com","b82a476b3b9e9d","0de723ba", "heroku_3e0e4fe3001638d") or die ("Невозможно подключиться к серверу");
  $zapros="DELETE FROM dk WHERE id=" . $_GET['id'];
  mysqli_query($conn, $zapros);
  header("Location: index.php");
